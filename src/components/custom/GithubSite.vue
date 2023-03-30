0f4422a376ff565850021d71d6d062b496e51308
linjiangyu <linjiangyu0702@linjiangyu.com>
2023-03-30 12:27:44 +0800

    none

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>Follow My</span>
+    <a href="https://linjiangyu.com" target="_blank" class="text-blue-500">
+      BLOG
     </a>
   </div>
 </template>
