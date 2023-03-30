03acb934ecb7607b3ecea2e4478d23eafa44648c
linjiangyu <linjiangyu0702@linjiangyu.com>
2023-03-30 12:16:09 +0800

    none

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://cdn1.tianli0.top/gh/linjiangyu2/halo/img/fa.jpg',
+      name: 'Tianlin',
+      description: 'Star on <a href="Tianlin's Private ChatGpt" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
